a179b2b952b699648ec3e3f0c1ace831922662fa
Andrey Filchenkov <afi@sitecore.net>
2016-12-14 15:52:58 +0200

    Initial release

---
+++ src/Sitecore.Support.137073/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using Sitecore.Diagnostics;
+using System;
+using System.Reflection;
+
+namespace Sitecore.Support.Data.Items
+{
+    public static class ItemExtensions
+    {
+        public static string GetFieldValue(this Sitecore.Data.Items.Item item, string fieldName)
+        {
+            Sitecore.Data.Fields.Field field = item.Fields[fieldName];
+
+            if (item.Versions.Count > 0 || field.Shared || !item.IsClone)
+                return field.ToString();
+            else
+                return ItemExtensions.GetFieldValue(field);
+        }
+        private static string GetFieldValue(Sitecore.Data.Fields.Field field)
+        {
+            string fieldValue = field.GetValue(true, true);
+            if (!field.InheritsValueFromOtherItem)
+                return fieldValue ?? String.Empty;
+
+            fieldValue = field.GetStandardValue();
+            if (fieldValue != null)
+            {
+                PropertyInfo propertyInfo = field.GetType().GetProperty("containsStandardValue");
+                if (propertyInfo != null)
+                    propertyInfo.SetValue(field, 1);
+                else
+                    Log.Error("The 'containsStandardValue' cannot be null.", field);
+                return fieldValue;
+            }
+            return field.GetValue(false, true) ?? String.Empty;
+        }
+    }
+}
---
+++ src/Sitecore.Support.137073/GetFieldValue.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.RenderField;
+using Sitecore.WordOCX;
+using System.Reflection;
+using Sitecore.Support.Data.Items;
+
+namespace Sitecore.Support.Pipelines.RenderField
+{
+    public class GetFieldValue
+    {
+        public void Process(RenderFieldArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            // Sitecore.Support +++
+            if (Sitecore.Context.PageMode.IsPreview || Sitecore.Context.PageMode.IsPageEditor)
+            {
+                PropertyInfo fieldValuePropertyInfo = args.GetType().GetProperty("FieldValue");
+                if (fieldValuePropertyInfo != null)
+                    fieldValuePropertyInfo.SetValue(args, args.Item.GetFieldValue(args.FieldName));
+                else
+                    Log.Error("The 'fieldValuePropertyInfo' instance cannot be null.", this);
+            }
+            args.Result.FirstPart = args.FieldValue;
+            // Sitecore.Support ---
+            if (args.FieldTypeKey == "rich text")
+            {
+                WordFieldValue value2 = WordFieldValue.Parse(args.Result.FirstPart);
+                if (value2.BlobId != ID.Null)
+                {
+                    args.Result.FirstPart = value2.GetHtmlWithStyles();
+                }
+            }
+        }
+    }
+}
